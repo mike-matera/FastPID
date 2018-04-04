081f747e99aa5ec81c32e228a2bea9b935dc4e4d
Mike Matera <maximus@fatboycentral.com>
2018-04-04 16:20:33 -0700

    Add a simple example.

---
+++ examples/PID_Basic/PID_Basic.ino
@@ -0,0 +1,31 @@
+#include <FastPID.h>
+
+#define PIN_INPUT A1
+#define PIN_OUTPUT 10
+
+double Kp=0, Ki=0, Kd=1;
+uint16_t deadband = 0; 
+int output_bits = 8; 
+bool output_signed = false;
+
+FastPID myPID(Kp, Ki, Kd, deadband, output_bits, output_signed);
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int setpoint = 500; 
+  int feedback = analogRead(PIN_INPUT);
+  int output = myPID.step(setpoint, feedback);
+  analogWrite(PIN_OUTPUT, 255 - output);
+  Serial.print("sp: "); 
+  Serial.print(setpoint); 
+  Serial.print(" fb: "); 
+  Serial.print(feedback);
+  Serial.print(" out: ");
+  Serial.println(output);
+  delay(100);
+}
