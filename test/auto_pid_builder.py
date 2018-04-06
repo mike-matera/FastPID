12b8f801036a5d034a07aa5d47268a5b6a16ed9f
Mike Matera <maximus@fatboycentral.com>
2018-04-06 10:19:19 -0700

    Build AudoPID for testing

---
+++ test/auto_pid_builder.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python3
+
+import distutils.sysconfig 
+from distutils.core import setup, Extension
+
+# build the harness. 
+ext = Extension('AutoPID',
+                     sources = ['auto_pid_reference.cpp', 'AutoPID/AutoPID.cpp'],
+                     include_dirs = ['AutoPID/']
+)
+
+# install
+setup (name = 'AutoPID',
+       version = '1.0',
+       description = 'Test harness for my a reference PID controller',
+       ext_modules = [ext],
+       script_args = ['install', '--install-platlib=./']
+)
+
+# test
+import AutoPID
---
+++ test/auto_pid_reference.cpp
@@ -0,0 +1,76 @@
+#include <Python.h>
+#include "AutoPID/AutoPID.h"
+#include <iostream>
+
+double Setpoint, Input, Output;
+double accumulator;
+
+AutoPID pid(&Input, &Setpoint, &Output, 0, 0, 0, 0, 0);
+
+static PyObject *
+configure(PyObject *self, PyObject *args) {
+  float kp;
+  float ki;
+  float kd; 
+  float db=0.0;
+  int bits=16; 
+  bool sign=false;
+  
+  if (!PyArg_ParseTuple(args, "ffffib", &kp, &ki, &kd, &db, &bits, &sign))
+    return NULL;
+
+  int64_t outmax, outmin;
+  if (bits > 16 || bits < 1) {
+    return PyBool_FromLong(false);
+  }  
+  else {
+    if (sign) {
+      outmax = ((0x1ULL << (bits - 1)) - 1);
+      outmin = -((0x1ULL << (bits - 1)));
+    }
+    else {
+      outmax = ((0x1ULL << bits) - 1);
+      outmin = 0;
+    }
+  }
+
+  accumulator = 0;
+  pid.setGains(kp, ki, kd);
+  pid.setTimeStep(1000); 
+  pid.setOutputRange(outmin, outmax);
+  return PyBool_FromLong(true);
+}
+
+static PyObject *
+step(PyObject *self, PyObject *args) {
+  int sp; 
+  int fb;
+  if (!PyArg_ParseTuple(args, "ii", &sp, &fb))
+    return NULL;
+
+  Setpoint = sp;
+  Input = fb; 
+  pid.run();
+  accumulator += Output;
+  return PyLong_FromLong(accumulator);
+}
+
+static PyMethodDef PIDMethods[] = {
+    {"configure",  configure, METH_VARARGS, "Configure the PID."},
+    {"step",  step, METH_VARARGS, "Run a PID step."},
+    {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef pid_module = {
+   PyModuleDef_HEAD_INIT,
+   "AutoPID",   /* name of module */
+   NULL, /* module documentation, may be NULL */
+   -1,       /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
+   PIDMethods
+};
+
+PyMODINIT_FUNC
+PyInit_AutoPID(void)
+{
+  return PyModule_Create(&pid_module);
+}
